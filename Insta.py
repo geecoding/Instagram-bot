6b30f0a43da8c53f06b124300fd7aa94493bd16a
geecoding <86950417+geecoding@users.noreply.github.com>
2021-07-05 16:10:01 +0530

    Update Insta.py

--- Insta.py
+++ Insta.py
@@ -4,7 +4,9 @@ bot = Bot()
 
 bot.login(username = "", password = "") #your username and password must be there
 
-bot.upload_photo("F:/animals/forest.jpg", caption="#saveforests #saveplanet #saveplanetearth")
+bot.send_message(text = "", user_ids = [""]) #your message content and user ids must be there. User id is whom do you want to send the message.
+
+bot.upload_photo("", captions = "") #your photo path like "F:/FF/FF" and captions is the title of the photo
 
 #Please subscribe 
 #https://youtube/Zd4MpUK8E2g
