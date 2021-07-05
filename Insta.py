02ad7cc397871cca34db367d9c37ce6427551847
geecoding <86950417+geecoding@users.noreply.github.com>
2021-07-05 13:40:50 +0530

    Update Insta.py

--- Insta.py
+++ Insta.py
@@ -5,3 +5,6 @@ bot = Bot()
 bot.login(username = "save_everythin", password = "Preet54321#")
 
 bot.upload_photo("F:/animals/forest.jpg", caption="#saveforests #saveplanet #saveplanetearth")
+
+#Please subscribe 
+https://youtube/Zd4MpUK8E2g
